5d3a47f3a49fb60e6f95f4ef064f65000ca2e538
lance <784532927@qq.com>
2017-07-31 22:57:57 +0800

    首页tab导航
    首页tab导航

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import MHeader from 'components/m-header/m-header.vue'
+  import Tab from 'components/tab/tab'
   export default {
     components: {
-      MHeader
+      MHeader,
+      Tab
     }
   }
 </script>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>singer</div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>

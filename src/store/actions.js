b018c2be6bcd1fcf3dfcb40013fa97d86646e932
liuxin <784532927@qq.com>
2017-08-18 11:23:25 +0800

    随机播放全部状态下选择播放

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,11 +5,34 @@
 import * as types from './mutation-types'
 import {playMode} from 'common/js/config'
 import {shuffle} from 'common/js/util'
+
+function findIndex (list, song) {
+  return list.findIndex((currentSong) => {
+    return currentSong.id === song.id
+  })
+}
+
+/**
+ * 歌手页面点击歌曲的action
+ * @param commit
+ * @param state
+ * @param list
+ * @param index
+ */
 export const selectPlay = function ({commit, state}, {list, index}) {
   // 提交mutation
-  commit(types.SET_SEQUENCELIST, list)
-  commit(types.SET_PLAYLIST, list)
+  // 判断当前的播放状态
+  if (state.mode === playMode.random) {
+    // 改变当前的playlist和currentindex
+    let randomList = shuffle(list)
+    commit(types.SET_PLAYLIST, randomList)
+
+    index = findIndex(randomList, list[index])
+  } else {
+    commit(types.SET_PLAYLIST, list)
+  }
   commit(types.SET_CURRENTINDEX, index)
+  commit(types.SET_SEQUENCELIST, list)
   commit(types.SET_FULLSCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
 }

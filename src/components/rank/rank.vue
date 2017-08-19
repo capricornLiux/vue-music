8d7d59a2cd020e00b254284a4c50f6b3b842b928
liuxin <784532927@qq.com>
2017-08-19 20:22:20 +0800

    排行首页基本功能

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,27 +1,48 @@
 <template>
-  <div class="rank">
-    <div class="toplist">
+  <div class="rank" :data="topList" ref="topListWrapper">
+    <scroll class="toplist" :data="topList" ref="topListScroll">
       <ul>
-        <li class="item">
+        <li class="item" v-for="item in topList">
           <div class="icon">
-            <img alt="" width="100" height="100">
+            <img alt="" width="100" height="100" v-lazy="item.picUrl">
           </div>
           <ul class="songlist">
-            <li class="song">
-              <span></span>
-              <span></span>
+            <li class="song" v-for="(song, index) in item.songList">
+              <span>{{index + 1}}</span>
+              <span>{{song.songname}}-{{song.singername}}</span>
             </li>
           </ul>
         </li>
       </ul>
-    </div>
+
+      <div class="loading-container" v-show="!topList.length">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import {getTopList} from 'api/rank'
   import {ERR_OK} from 'api/config'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+  import {playListMixin} from 'common/js/mixin'
+  import {mapGetters} from 'vuex'
+
   export default {
+    mixins: [
+      playListMixin
+    ],
+    components: {
+      Scroll,
+      Loading
+    },
+    data () {
+      return {
+        topList: []
+      }
+    },
     created () {
       this._getTopList()
     },
@@ -29,9 +50,18 @@
       _getTopList () {
         getTopList().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res)
+            console.log(res.data.topList)
+            this.topList = res.data.topList
           }
         })
+      },
+      ...mapGetters([
+        'playList'
+      ]),
+      handlePlayList (playList) {
+        const bottom = playList.length > 0 ? '60px' : ''
+        this.$refs.topListWrapper.style.bottom = bottom
+        this.$refs.topListScroll.refresh()
       }
     }
   }

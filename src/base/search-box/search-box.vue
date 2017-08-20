154650464108d4b0529b3b7c28261ac3b22616c4
liuxin <784532927@qq.com>
2017-08-20 23:16:46 +0800

    搜索tab页面,search-box基础组件开发

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input type="text" class="box" :placeholder="placeholder" v-model="query">
+    <i class="icon-dismiss" v-show="query" @click="clearQuery"></i>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      placeholder: {
+        type: String,
+        default: '搜索歌曲、歌手'
+      }
+    },
+    data () {
+      return {
+        query: ''
+      }
+    },
+    methods: {
+      clearQuery () {
+        this.query = ''
+      }
+    },
+    created () {
+      // 监听this.query的改变, 基础组件只派发事件给外界, 不处理其他逻辑
+      this.$watch('query', (newValue) => {
+        this.$emit('queryChange', newValue)
+      })
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .search-box
+    display: flex
+    align-items: center
+    box-sizing: border-box
+    width: 100%
+    padding: 0 6px
+    height: 40px
+    background: $color-highlight-background
+    border-radius: 6px
+    .icon-search
+      font-size: 24px
+      color: $color-background
+    .box
+      flex: 1
+      margin: 0 5px
+      line-height: 18px
+      background: $color-highlight-background
+      color: $color-text
+      font-size: $font-size-medium
+      &::placeholder
+        color: $color-text-d
+    .icon-dismiss
+      font-size: 16px
+      color: $color-background
+</style>
